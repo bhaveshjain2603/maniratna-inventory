12e80a2e931b19a5af500e5afdf9db566a1a6be7
Bhavesh Jain <bhaveshkocheta50@gmail.com>
2026-06-11 11:44:56 +0530

    Updated files

---
+++ backend/config/passport.js
@@ -0,0 +1,25 @@
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+
+app.use(passport.initialize());
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL:
+        '/api/auth/google/callback',
+    },
+    async (accessToken, refreshToken, profile, done) => {
+
+      const user = {
+        googleId: profile.id,
+        name: profile.displayName,
+        email: profile.emails[0].value,
+      };
+
+      done(null, user);
+    }
+  )
+);
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,41 @@
 import express from 'express';
+import passport from 'passport';
 import { login, register, getCurrentUser, logout } from '../controllers/authController.js';
 import { authenticate } from '../middleware/auth.js';
 
 const router = express.Router();
+
+router.get(
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+);
+
+router.get(
+  '/google/callback',
+  passport.authenticate('google', {
+    session: false,
+  }),
+  async (req, res) => {
+
+    const token = jwt.sign(
+      {
+        id: req.user.googleId,
+        name: req.user.name,
+        email: req.user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: '1d',
+      }
+    );
+
+    res.redirect(
+      `https://dashboard.maniratnajewels.in/auth-success?token=${token}`
+    );
+  }
+);
 
 router.post('/login', login);
 router.post('/register', register);
