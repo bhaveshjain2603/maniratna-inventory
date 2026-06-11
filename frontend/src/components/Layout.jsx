fc3a2f447d8e6850c11e3c0be055384acc88f825
Bhavesh Jain <bhaveshkocheta50@gmail.com>
2026-06-11 15:07:13 +0530

    Updated files

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -60,9 +60,16 @@ const Layout = () => {
       >
         <div className="p-3 md:p-4 border-b border-gray-700 flex items-center justify-between flex-shrink-0">
           {sidebarOpen && (
-            <span className="text-sm md:text-lg">Welcome,</span>
+            <div className="flex flex-col">
+              <span className="text-sm md:text-lg">
+                Welcome,
+              </span>
+
+              <span className="text-lg md:text-base font-semibold">
+                {user?.name}
+              </span>
+            </div>            
           )}
-          <span className="text-lg md:text-2xl">{user?.name}</span>
           {sidebarOpen && isMobile && (
             <button
               onClick={() => setSidebarOpen(false)}
