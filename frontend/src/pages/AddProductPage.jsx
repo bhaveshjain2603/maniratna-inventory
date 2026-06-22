94dbe53c230568a7614cfda627c1f500540a252d
Bhavesh Jain <bhaveshkocheta50@gmail.com>
2026-06-22 15:11:57 +0530

    Updated files

--- frontend/src/pages/AddProductPage.jsx
+++ frontend/src/pages/AddProductPage.jsx
@@ -97,7 +97,9 @@ const AddProductPage = () => {
 
       toast.success("Product added successfully!");
 
-      navigate("/products");
+      setTimeout(() => {
+        navigate("/products");
+      }, 1200);
     } catch (err) {
       console.error("Create Product Error:", err);
 
