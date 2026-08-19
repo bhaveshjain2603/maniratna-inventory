b9b4137a13d96cd20e16ebf5775af8296e4d9865
Bhavesh Jain <126448088+bhaveshjain2603@users.noreply.github.com>
2026-08-19 11:43:23 +0530

    Update ProductsPage.jsx

--- frontend/src/pages/ProductsPage.jsx
+++ frontend/src/pages/ProductsPage.jsx
@@ -19,7 +19,7 @@ const ProductsPage = () => {
   const [showActionModal, setShowActionModal] = useState(false);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
 
-  const bulkCategories = ["Earrings", "Baby Rings"];
+  const bulkCategories = ["Earrings", "Gents Ring (Without Tag)", "Ladies Ring (Without Tag)", "Baby Rings"];
 
   useEffect(() => {
     fetchProducts();
