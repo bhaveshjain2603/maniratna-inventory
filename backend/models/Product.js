94474b5005c46db4380b5ff4b94be0f473b4bd45
Bhavesh Jain <126448088+bhaveshjain2603@users.noreply.github.com>
2026-08-19 11:29:19 +0530

    Update Product.js

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -15,7 +15,9 @@ const productSchema = new mongoose.Schema(
       enum: [
         'Earrings',
         'Gents Ring',
+        'Gents Ring (Without Tag)',
         'Ladies Ring',
+        'Ladies Ring (Without Tag)',
         'Baby Rings',
         'Couple Ring',
         'God Ring',
